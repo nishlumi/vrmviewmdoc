d8cc88a9412e0ac0571ce65e357fa0950589d323
nishlumi <nishlumi@users.noreply.github.com>
2023-09-02 23:15:01 +0900

    2023/09/02夜時点
    For ver 2.1.6 - 20230902

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, ISHII Eiju'
 author = 'ISHII Eiju'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.2'
+release = '2.1.6'
 
 
 # -- General configuration ---------------------------------------------------
