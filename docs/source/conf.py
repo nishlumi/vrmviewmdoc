cbefa63f7fd92dd42867b39129a56544ba95fc0c
nishlumi <nishlumi@users.noreply.github.com>
2025-12-26 20:24:02 +0900

    2025.12.26夜時点
    For ver 2.19.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, ISHII Eiju'
 author = 'ISHII Eiju'
 
 # The full version, including alpha/beta/rc tags
-release = '2.18.0'
+release = '2.19.0'
 
 
 # -- General configuration ---------------------------------------------------
